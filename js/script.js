02e26be4b8f8a7cbc0e00d91d3df98d912640d26
Manuel-Lauriola <manuelauriol@gmail.com>
2025-07-01 15:56:44 +0200

    [UPD]-questions-to-user

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+
+const age = prompt(`inserisci la tua età`)
+const trip = prompt(`inserisci la distanza in km`)
